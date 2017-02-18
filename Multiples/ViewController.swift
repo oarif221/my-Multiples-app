c19d0e2eac9b6fb78dea832d2abdc3b8de2181a6
Omer Arif <oamsak@gmail.com>
2017-02-18 13:53:13 +0100

    adding all multiples files

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    var number = 0
+    var counterNumber = 0
+    var result = 0
+    var text = ""
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var multiples: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var pressAddText: UITextView!
+    @IBOutlet weak var addButton: UIButton!
+    
+    
+    @IBAction func playBtn(sender: UIButton!){
+        
+        if textField.text != nil && textField.text != "" {
+            multiples.isHidden = true
+            textField.isHidden = true
+            playButton.isHidden = true
+            addButton.isHidden = false
+            pressAddText.isHidden = false
+            text = pressAddText.text
+            number = Int(textField.text!)!
+            result = number
+        }
+        
     }
-
+    
+    @IBAction func addBtn(sender: UIButton!){
+        
+        
+        if result < 60 {
+            result = counterNumber + number
+            pressAddText.text = "\(counterNumber) + \(number) = \(result)"
+            counterNumber = result
+            
+        } else {
+            multiples.isHidden = false
+            textField.isHidden = false
+            playButton.isHidden = false
+            addButton.isHidden = true
+            pressAddText.isHidden = true
+            textField.text = ""
+            pressAddText.text = text
+            counterNumber = 0
+            result = 0
+        }
+        
+    }
 
 }
 
